2af3702f2c023fc5ca5187c213b63af1a0d24786
yusra_suhail@uri.edu <Yusra Suhail>
2024-09-05 01:38:34 -0400

    updated interface and added clerk

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
 "use client";
-import { Box, Button, Stack, TextField } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Stack,
+  TextField,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material";
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home() {
   // state for managing messages and user input
@@ -53,13 +64,86 @@ export default function Home() {
   };
   return (
     <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
+      sx={{
+        my: 0,
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#d9dde8",
+        height: "100vh",
+        gap: 5,
+      }}
       alignItems="center"
     >
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "#d9dde8",
+        }}
+      >
+        <Toolbar>
+          <a href="./">
+            <IconButton
+              sx={{
+                transition: "box-shadow 0.3s ease",
+                "&:hover": {
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+                },
+                padding: 0, // Remove padding to fit image
+              }}
+            >
+              <Image
+                src="/review_icon.png" // Path to your image in the public folder
+                alt="Review Icon"
+                width={50}
+                height={50}
+              />
+            </IconButton>
+          </a>
+          <Typography
+            variant={"h4"}
+            color={"#000"}
+            sx={{ flexGrow: 1, fontWeight: 800 }}
+          >
+            Professor Review Intel
+          </Typography>
+          <SignedOut>
+            <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+              <Button
+                variant="contained"
+                sx={{
+                  border: "4px solid #4255ff",
+                  backgroundColor: "#4255ff", // Custom background color
+                  color: "#FFFFFF", // Custom text color
+                  "&:hover": {
+                    backgroundColor: "#4255ff", // Custom hover background color
+                  },
+                }}
+                href="/sign-in"
+              >
+                Login
+              </Button>
+              <Button
+                color="inherit"
+                variant="contained"
+                sx={{
+                  border: "4px solid #4255ff",
+                  backgroundColor: "#4255ff", // Custom background color
+                  color: "#FFFFFF", // Custom text color
+                  "&:hover": {
+                    backgroundColor: "#4255ff", // Custom hover background color
+                  },
+                }}
+                href="/sign-up"
+              >
+                Sign Up
+              </Button>
+            </Box>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
       <Stack
         direction={"column"}
         width="500px"
